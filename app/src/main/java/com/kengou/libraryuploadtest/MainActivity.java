ac76c963a1888e5a805b8e51fda1a803ee24b986
hanjianhao <hanjianhao@tpson.cn>
2018-09-26 14:37:18 +0800

    初次提交

---
+++ app/src/main/java/com/kengou/libraryuploadtest/MainActivity.java
@@ -0,0 +1,22 @@
+package com.kengou.libraryuploadtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.kengou.librarytest.LibraryTest;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.tv);
+
+        tv.setText(LibraryTest.getMyTestStr());
+    }
+}
---
+++ librarytest/src/main/java/com/kengou/librarytest/LibraryTest.java
@@ -0,0 +1,8 @@
+package com.kengou.librarytest;
+
+public class LibraryTest {
+
+    public static String getMyTestStr(){
+        return "Hello, I'm the test string!";
+    }
+}
